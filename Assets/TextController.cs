c8153002167e893db7382b141c07d42913cbe0b4
Michael Budnick <Michael@michael.com>
2016-11-01 19:16:55 -0400

    Everything fixed to corridor

--- Assets/TextController.cs
+++ Assets/TextController.cs
@@ -7,12 +7,14 @@ public class TextController : MonoBehaviour {
 
 	public Text text;
 
-	private enum States {intro, cell, mirror, getMirror, getSheets, bed, cellDoor, leaveCell, openCell};
+	private enum States {intro, cell, mirror, getMirror, getSheets, bed, cellDoor, leaveCell, openCell,
+											corridor, closet, insideCloset, guard, stairwell, bathroom, stalls, shower, outsideDoor};
 	private States myState 		 = States.intro;
 	private bool gotSheets 		 = false;
 	private bool gotMirror 		 = false;
 	private bool leftCell 		 = false;
 	private bool gotJanitorKey = false;
+	private bool closetOpen		 = false;
 	private bool gotRag 			 = false;
 	private bool cleanedShower = false;
 	private bool gotBroom 		 = false;
@@ -42,7 +44,7 @@ public class TextController : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		if (myState == States.intro && Input.GetKeyDown(KeyCode.Bpace)) {myState = States.cell;}
+		if (myState == States.intro && Input.GetKeyDown(KeyCode.Space)) {myState = States.cell;}
 		else if (myState == States.cell) 			{cell();}
 		else if (myState == States.mirror) 		{mirror();}
 		else if (myState == States.bed) 		{bed();}
@@ -64,7 +66,7 @@ public class TextController : MonoBehaviour {
 				cellNoMirror();
 		} else if (!leftCell && !gotMirror && gotSheets) {
 				cellNoSheets();
-		} else if (!leftcell && gotMirror && gotSheets) {
+		} else if (!leftCell && gotMirror && gotSheets) {
 			cellNoSheetsNoMirror();
 		}
 
@@ -77,7 +79,7 @@ public class TextController : MonoBehaviour {
 		text.text = "There are some dirty bed on the bed, " +
 		 						"a space where the mirror was on the wall, " +
 								"and a door that is locked from the outside.\n\n" +
-								"Press S to view the bed\n" +
+								"Press B to view the Bed\n" +
 								"Press M to view the Mirror space\n" +
 								"Press D to view the Door";
 	}
@@ -86,7 +88,7 @@ public class TextController : MonoBehaviour {
 		text.text = "There is a mattress that appears to have a sheet fused to it, " +
 								"a mirror on the wall, " +
 								"and a door that is locked from the outside.\n\n" +
-								"Press S to view the Mattress\n" +
+								"Press B to view the Bed\n" +
 								"Press M to view the Mirror\n" +
 								"Press D to view the Door";
 	}
@@ -95,13 +97,13 @@ public class TextController : MonoBehaviour {
 		text.text = "There is a mattress that appears to have a sheet fused to it, " +
 								"a space where the mirror was on the wall, " +
 								"and a door that is locked from the outside.\n\n" +
-								"Press S to view the Mattress\n" +
+								"Press B to view the Bed\n" +
 								"Press M to view the Mirror\n" +
 								"Press D to view the Door";
 	}
 
 	void mirror () {
-		if (!gotMirror) {
+		if (!gotMirror && !gotSheets) {
 			text.text = "You look into the mirror at a pathetic pale person soaked in a cold sweat.\n" +
 									"How could you this have happened? You've never done wrong before. " +
 									"You promised not to tell a soul about what you saw, but how could you not? " +
@@ -110,14 +112,21 @@ public class TextController : MonoBehaviour {
 									"Press R to Rip the mirror from the wall!";
 
 									if (Input.GetKeyDown(KeyCode.R)) 	{myState = States.getMirror;}
+		} else if (!gotMirror && gotSheets){
+			text.text = "You look into the mirror at a pathetic pale person wrapped in a tasteful toga soaked in a cold sweat.\n" +
+									"How could you this have happened? You've never done wrong before. " +
+									"You promised not to tell a soul about what you saw, but how could you not? " +
+									"They're not going to let you out of here, you have to break out and flee.\n\n" +
+									"Press C to go back to the middle of the Cell\n" +
+									"Press R to Rip the mirror from the wall!";
+
+			if (Input.GetKeyDown(KeyCode.R)) 	{myState = States.getMirror;}
 		} else if (gotMirror){
 				text.text = "There is a light rectangular mark with screw holes in it from where " +
 										"the mirror used to be. Someone, (the installer?) has carved a doggerel behind it:\n\n" +
 										"I once ate dinner at the master's house,\nAfter eating he was straight to bed\n" +
 										"His wife stayed and dropped me her blouse.\nHoley moley I near dropped dead!\n" +
-										"\n\nPress C to go back to the middle of the cell.";
-		} else if (!gotMirror && gotSheets){
-
+										"\n\nPress C to go back to the middle of the Cell.";
 		}
 
 		if (Input.GetKeyDown(KeyCode.C)) 	{myState = States.cell;}
@@ -130,7 +139,7 @@ public class TextController : MonoBehaviour {
 								"Press SPACE";
 		gotMirror = true;
 
-		if (Input.GetKeyDown(KeyCode.Bpace)) {myState = States.cell;}
+		if (Input.GetKeyDown(KeyCode.Space)) {myState = States.cell;}
 	}
 
 	void bed () {
@@ -138,13 +147,13 @@ public class TextController : MonoBehaviour {
 			text.text = "There's a sheet tucked around the mattress and another on top of that. " +
 									"They're both an off-white, an oily yellow. They definitely should be changed.\n" +
 									"There's neither window or rafter to make any use of them, unwashed togas maybe.\n" +
-									"\n\nPress C to go back to the middle of the cell.\n" +
-									"Press S to grab the sheets."
+									"\n\nPress C to go back to the middle of the Cell.\n" +
+									"Press S to grab the Sheets.";
 									if (Input.GetKeyDown(KeyCode.S)) 	{myState = States.getSheets;}
 		} else {
 			text.text = "There's a sheet tucked around the mattress, you tried to take it off but it was really starchy, and stuck to the mattress like glue. " +
 									"It definitely should be changed, but it doesn't look it it can be.\n" +
-									"\n\nPress C to go back to the middle of the cell.\n"
+									"\n\nPress C to go back to the middle of the Cell.\n";
 		}
 
 		if (Input.GetKeyDown(KeyCode.C)) 	{myState = States.cell;}
@@ -154,13 +163,13 @@ public class TextController : MonoBehaviour {
 		if (!gotMirror) {
 				text.text = "It's a big iron door with a slot they slide the food tray through for meals. " +
 										"The food's not bad, but they don't use enough salt. Anyways, yup, the door's still locked.\n" +
-										"\n\nPress C to go back to the middle of the cell.";
+										"\n\nPress C to go back to the middle of the Cell.";
 		} else {
 			text.text = "The mirror is a good size for putting out the food tray slot...\n" +
 									"You can see the reflection down the hall, the guard is sleeping at his desk, he even brings in a neck pillow. " +
 									"You look down at the door handle... the lock is just a latch, you could easily reach out and let yourself out. " +
 									"It can't be that easy, can it?" +
-									"\n\nPress C to go back to the middle of the cell." +
+									"\n\nPress C to go back to the middle of the Cell." +
 									"\nPress Space to go out the door.";
 
 			if (Input.GetKeyDown(KeyCode.Space)) 	{myState = States.leaveCell;}
@@ -173,7 +182,7 @@ public class TextController : MonoBehaviour {
 		text.text = "You take the sheet off the bed and wrap them around yourself, tying it at the shoulder. " +
 								"You look at yourself in the mirror and dream of a cell block toga party... Too bad " +
 								"there's no time to dream, you have to get out of here!" +
-								"\n\nPress C to go back to the middle of the cell.";
+								"\n\nPress C to go back to the middle of the Cell.";
 		gotSheets = true;
 
 		if (Input.GetKeyDown(KeyCode.C)) 	{myState = States.cell;}
@@ -181,12 +190,12 @@ public class TextController : MonoBehaviour {
 
 	void leaveCell () {
 		text.text = "You slide your hand out the slot in the door and lift up the latch. " +
-					"You can already feel the door start opening before you even push it... " +
-					"Can it really be this simple? Download the Prison Break DLC for $2.99 to find out!" +
-					"\nPress Space to continue.";
+								"You can already feel the door start opening before you even push it... " +
+								"Can it really be this simple? Download the Prison Break DLC for $2.99 to find out!" +
+								"\n\nPress Space to continue.";
 		leftCell = true;
 
-		if (Input.GetKeyDown(KeyCode.space)) 	{myState = States.openCell;}
+		if (Input.GetKeyDown(KeyCode.Space)) 	{myState = States.openCell;}
 	}
 
 	void openCell () {
@@ -195,5 +204,8 @@ public class TextController : MonoBehaviour {
 								"and clangs against the wall. The noise is loud and echoes through the corridor. " +
 								"The guard down the hall snorts and bit, but he doesn't wake up. He's out cold.\n\n" +
 								"Press SPACE to continue into the corridor.";
+
+		if (Input.GetKeyDown(KeyCode.Space)) 	{myState = States.corridor;}
 	}
+
 }
